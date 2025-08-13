ea6492fbf7e693845a7a57a6649074f4152f17cd
MuthuYogesh <muthuyogeshb@gmail.com>
2025-08-13 16:47:29 +0530

    Setup Push

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="ml-50 mr-50 text-4xl text-white bg-red-500 mt-10 p-10 rounded-lg w-full">
+        under Build
+      </h1>
+      {/* <Navbar></Navbar>
+      <Herosection></Herosection>
+      <Projects></Projects>
+      <Contact></Contact>
+      <Footer></Footer> */}
+    </>
+  );
+}
+
+export default App
