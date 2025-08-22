07e20530781faac15b4ea106efdc3a4155c99593
MuthuYogesh <muthuyogeshb@gmail.com>
2025-08-22 20:08:54 +0530

    Logo changed, Footer Altered, public folder updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="w-full py-1 bg-background text-center text-gray-400 text-sm border-t border-gray-700">
-            © {new Date().getFullYear()} All rights reserved Muthu Yogesh.
+            © {new Date().getFullYear()} All rights reserved.
         </footer>
     );
 }
